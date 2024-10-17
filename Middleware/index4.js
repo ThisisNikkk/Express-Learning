1bde1e3337d53690e7c72a50202875d080f6557a
Nikhil Siwan <developer.nikk@gmail.com>
2024-10-17 22:57:22 +0530

    Practising HTML Requests

--- Middleware/index4.js
+++ Middleware/index4.js
@@ -1,30 +1,29 @@
-import express from "express";
-import bodyParser from "body-parser";
-import {dirname} from "path";
+import express from "express"
+import{dirname} from "path";
 import { fileURLToPath } from "url";
-const _dirname = dirname(fileURLToPath(import.meta.url));
-const app = express ();
+import bodyParser from "body-parser";
+const dir_name = dirname(fileURLToPath(import.meta.url));
+const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
-var brandName = "";
 
-function brandNameGenerator(req,res,next){
-  console.log(req.body);
-  brandName = req.body["street"] + req.body["pet"];
-  next();
+var brandname = "";
+
+const brandnamegenerator = (req,res,next) => {
+  brandname = req.body["street"] + req.body["pet"];
+  next()
 }
 
-app.use(brandNameGenerator);
+app.use(brandnamegenerator);
 
 app.get("/", (req,res)=>{
-  res.sendFile(_dirname + "/public/index.html")
-});
+  res.sendFile(dir_name + "/public/index.html")
+})
 
 app.post("/submit", (req,res)=>{
-  res.send(`<h1>Your Brand Name is : ${brandName}</h1>`);
-});
+  res.send(`<h1> Your Brand Name Is : <br> ${brandname}</h1>`)
+})
 
-
-app.listen(port, (req,res)=>{
-  console.log("Server is running")
-});
+app.listen(port, ()=>{
+  console.log(`Your port is running at ${port}`);
+})
