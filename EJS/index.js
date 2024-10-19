82ae75c47b582fbf85d3dfcad9fc2caf98bdbe87
Nikhil Siwan <developer.nikk@gmail.com>
2024-10-20 00:01:58 +0530

    Bug Fixed

--- EJS/index.js
+++ EJS/index.js
@@ -4,9 +4,20 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req,res)=>{
+    const today = new Date();
+    const day = today.getDay();
+
+    let type = "WeekDays"
+    let adv = "Office Chala Ja"
+
+    if(day === 0 || day ===6){
+        type = "WeekEnd"
+        adv = "Chill Kar Bhai !!"
+    }
+
     res.render("index.ejs",{
-        dayName : "Monday",
-        advice : "Back To The Office !"
+        dayName : type,
+        advice : adv,
     })
 })
 
